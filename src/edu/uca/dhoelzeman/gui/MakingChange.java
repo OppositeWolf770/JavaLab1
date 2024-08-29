3237af5b5895868b676fbabf10b0605573dc2b1a
Daniel Hoelzeman <DHOELZEMAN@cub.uca.edu>
2024-08-29 00:07:32 -0500

    Customize the initial JFrame under MakingChange

--- src/edu/uca/dhoelzeman/gui/MakingChange.java
+++ src/edu/uca/dhoelzeman/gui/MakingChange.java
@@ -5,5 +5,7 @@ import javax.swing.*;
 public class MakingChange {
     public static void main() {
         var frame = new JFrame("Register");
+        frame.setSize(800, 600);
+        frame.setVisible(true);
     }
 }

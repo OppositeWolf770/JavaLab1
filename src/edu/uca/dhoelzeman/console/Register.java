95c1cb637eb06e1c52d53dd802c2712b11d62c33
Daniel Hoelzeman <DHOELZEMAN@cub.uca.edu>
2024-09-03 01:26:41 -0500

    Refactor the makeChange method to handle .005 values. Add an early return when amount <= 0.

--- src/edu/uca/dhoelzeman/console/Register.java
+++ src/edu/uca/dhoelzeman/console/Register.java
@@ -1,7 +1,9 @@
 package edu.uca.dhoelzeman.console;
 
-import edu.uca.dhoelzeman.gui.MakingChange;
+import edu.uca.dhoelzeman.console.CurrencyValues.Forms;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Scanner;
 
 /**
@@ -29,34 +31,47 @@ public class Register {
     /**
      * Converts the specified amount to their respective {@link Denomination} values
      *
-     * @param amt The amount of money to be converted
+     * @param amount The amount of money to be converted
      * @return A {@link Purse} containing the individual {@link Denomination} values from the provided money value
      *
      * @see Purse
      * @see Denomination
      */
-    public Purse makeChange(double amt) {
+    public Purse makeChange(double amount) {
         var purse = new Purse();
 
-        amt *= 100; // Convert amt to all cents
+        // If value is 0 or a negative number, return early
+        if (amount <= 0) {
+            return purse;
+        }
 
+        // Rounds decimals >= .005 up to a penny
+        amount = BigDecimal.valueOf(amount)
+                .setScale(2, RoundingMode.HALF_UP)
+                .doubleValue();
+
+        amount *= 100; // Convert amt to all cents for easier calculations
+
+        // Loops through each CurrencyValue and gets the correct amount of that denomination to add to the purse
         for (var currencyValue : CurrencyValues.values()) {
             // Prevent unnecessary code execution if amt is less than a Penny
-            if (amt < CurrencyValues.PENNY.value) {
+            if (amount < CurrencyValues.PENNY.value) {
                 break;
             }
 
             // currencyValue.value is multiplied by 100 here, because amt was converted to cents above
-            double new_amt = amt % (currencyValue.value * 100);
-            int num = (int) (amt / (currencyValue.value * 100)); // The number of that bill to put in consoleClasses.Purse
+            double remainingAmount = amount % (currencyValue.value * 100);
+            int num = (int) (amount / (currencyValue.value * 100)); // The number of that bill to put in Purse
 
+            // Add the denomination to the purse
             if (num != 0) {
-                var form = currencyValue.value >= 1 ? CurrencyValues.Forms.Bill : CurrencyValues.Forms.Coin;
-                var denomination = new Denomination(currencyValue.name, currencyValue.value, form, "NYI");
+                Forms form = currencyValue.value >= 1 ? Forms.Bill : Forms.Coin;
+                String img = "src/edu/uca/dhoelzeman/gui/images/" + currencyValue.name + ".png";
+                var denomination = new Denomination(currencyValue.name, currencyValue.value, form, img);
                 purse.add(denomination, num);
             }
 
-            amt = new_amt;
+            amount = remainingAmount;
         }
 
         return purse;

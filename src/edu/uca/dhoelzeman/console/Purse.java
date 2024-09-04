34df4baf15b8bdadc96238d016d14703cea6bb8e
Daniel Hoelzeman <DHOELZEMAN@cub.uca.edu>
2024-09-04 13:56:14 -0500

    Refactor toString() method. Implement remove() method.

--- src/edu/uca/dhoelzeman/console/Purse.java
+++ src/edu/uca/dhoelzeman/console/Purse.java
@@ -4,20 +4,20 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
- *
+ * Holds a Collection of Denominations and provides methods to modify
+ * the collection and print it out as a string
  */
 public class Purse {
+    // Holds the money that is in the purse and the amount of each denomination
+    // Uses a Link
     private final Map<Denomination, Integer> cash = new LinkedHashMap<>();
 
+    // Getter for the cash Map
     public Map<Denomination, Integer> getCash() {
         return cash;
     }
 
-    /** Adds the specified number of a denomination to the purse
-     * @param denomination The denomination to be added (100 Note, 1 Note, Quarter)
-     * @param num The number of that denomination to add to the purse
-     * @see Denomination
-     */
+    // Adds the specified number of a denomination to the purse
     public void add(Denomination denomination, int num) {
         if (cash.containsKey(denomination)) {
             cash.put(denomination, cash.get(denomination) + num);
@@ -27,27 +27,36 @@ public class Purse {
         cash.put(denomination, num);
     }
 
-    /** Removes the specified number of a denomination from the purse
-     * @param type The type of the denomination (100 Note, 1 Note, Quarter)
-     * @param num The number of that denomination to remove
-     * @return The amount in the purse after removal of a denomination amount
-     * @see Denomination
-     * @see CurrencyValues
-     */
-    public double remove(Denomination type, int num) {
-        double amt = getValue();
-
-        for (var denomination : cash.keySet()) {
-            cash.remove(denomination);
+    // Removes the specified number of a denomination from the purse
+    public double remove(Denomination denomination, int num) {
+        // If none of that denomination exists in the purse, do nothing and return the value of the purse
+        if (!cash.containsKey(denomination)) {
+            return getValue();
         }
 
-        return amt;
+        // Gets the state of the purse before and after removal
+        int currentAmount = getCash().get(denomination);
+        int newAmount = currentAmount - num;
+
+        // Remove the entire denomination if result is <= 0
+        if (newAmount <= 0) {
+            getCash().remove(denomination);
+        } else {
+            // Otherwise, just remove the indicated amount
+            getCash().put(denomination, newAmount);
+        }
+
+        // Return the value of the purse after the removal
+        return getValue();
     }
 
+    // Returns the total amount of money in the purse
     public double getValue() {
         double sum = 0;
 
-        for (var entry : cash.entrySet() ) {
+        // Loops through each denomination and adds its value to the sum
+        // according to the amount of that denomination in the purse
+        for (var entry : getCash().entrySet() ) {
             sum += entry.getKey().amt() * entry.getValue();
         }
 
@@ -55,28 +64,33 @@ public class Purse {
     }
 
     /**
+     * Called when the purse is sent to the output stream
      * @return The cash in the purse in an output friendly String format
      */
     public String toString() {
-        if (getValue() == 0) {
+        // Early return if the purse is empty
+        if (getCash().isEmpty()) {
             return "Empty Purse";
         }
 
-        StringBuilder builder = new StringBuilder();
+        // Contains the string block after denominations are added to it
+        var purseContents = new StringBuilder();
 
         // Loops through the denominations in the purse
         for (var denomination : cash.entrySet()) {
-            var name = denomination.getKey().name();
-            var num = denomination.getValue();
-            var form = denomination.getKey().form();
+            // Defines the values for the denomination in
+            // variables to make the string builder more readable
+            final String name = denomination.getKey().name();
+            final int num = denomination.getValue();
+            final String formMsg = denomination.getKey().form() == CurrencyValues.Forms.Bill ? " Note" : "";
 
             // Builds the registerOutput to be returned
-            builder.append(num).append(" ")
+            purseContents.append(num).append(" ")
                    .append(name)
-                   .append(form == CurrencyValues.Forms.Bill ? " Note" : "")
+                   .append(formMsg)
                    .append("\n");
         }
 
-        return builder.toString();
+        return purseContents.toString();
     }
 }

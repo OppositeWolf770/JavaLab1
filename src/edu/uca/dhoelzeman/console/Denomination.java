91e6dd2d81c42e69f94ed5b582cbf26f8972354b
Daniel Hoelzeman <DHOELZEMAN@cub.uca.edu>
2024-09-04 13:54:53 -0500

    Remove excessive documentation

--- src/edu/uca/dhoelzeman/console/Denomination.java
+++ src/edu/uca/dhoelzeman/console/Denomination.java
@@ -2,18 +2,6 @@ package edu.uca.dhoelzeman.console;
 
 /**
  * Holds the information for specific types of {@link CurrencyValues} for
- * use inside the {@link Purse}
- *
- * @see CurrencyValues
- * @see CurrencyValues#name
- * @see CurrencyValues.Forms
- * @see Register
- * @see Purse
- *
- * @param name The {@link String} representation of the denomination
- * @param amt The number of that denomination in the {@link Purse}
- * @param form The {@link CurrencyValues.Forms} representation of the denomination type
- * @param img The image for the specific denomination. Used for the GUI of the {@link Register}
+ * use inside the {@link Purse} class
  */
 public record Denomination(String name, double amt, CurrencyValues.Forms form, String img) {}
-

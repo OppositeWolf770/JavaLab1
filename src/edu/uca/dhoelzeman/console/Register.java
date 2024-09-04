c6fa2d9d3b20adc4058712b8fa031a85d481f184
Daniel Hoelzeman <DHOELZEMAN@cub.uca.edu>
2024-09-04 13:57:08 -0500

    Modify documentation and simplify makeChange function

--- src/edu/uca/dhoelzeman/console/Register.java
+++ src/edu/uca/dhoelzeman/console/Register.java
@@ -1,46 +1,47 @@
 package edu.uca.dhoelzeman.console;
 
-import edu.uca.dhoelzeman.console.CurrencyValues.Forms;
-
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-/**
- * Contains {@link #makeChange(double)}
- */
+
+ /**
+  * Main Class for the Console Program. Contains the main function,
+  * and the function to make the change for a provided Purse.
+  */
 public class Register {
     public static void main(String[] args) {
         var register = new Register();
         var scanner = new Scanner(System.in);
 
+        // Get user input
         System.out.print("Enter amount: ");
+
+        // Attempt to assign console input to amt
         double amt;
         try {
             amt = scanner.nextDouble();
-        } catch(Exception e) {
+        } catch(InputMismatchException e) {
+            // Exit early if user does not enter a Double
             System.out.println("Invalid input. Exiting...");
             return;
         }
 
+        // Make the change and output the purse contents to the console
         var purse = register.makeChange(amt);
         System.out.println(purse);
     }
 
 
-    /**
-     * Converts the specified amount to their respective {@link Denomination} values
-     *
-     * @param amount The amount of money to be converted
-     * @return A {@link Purse} containing the individual {@link Denomination} values from the provided money value
-     *
-     * @see Purse
-     * @see Denomination
-     */
+     /**
+      * Converts the specified amount to their respective denomination values and
+      * return a purse containing that amount
+      */
     public Purse makeChange(double amount) {
         var purse = new Purse();
 
-        // If value is 0 or a negative number, return early
+        // If amount is <= 0, return early
         if (amount <= 0) {
             return purse;
         }
@@ -59,18 +60,17 @@ public class Register {
                 break;
             }
 
-            // currencyValue.value is multiplied by 100 here, because amt was converted to cents above
+            // currencyValue.value is converted to cents to be used in calculations
             double remainingAmount = amount % (currencyValue.value * 100);
             int num = (int) (amount / (currencyValue.value * 100)); // The number of that bill to put in Purse
 
-            // Add the denomination to the purse
+            // Add the denomination to the purse if num != 0
             if (num != 0) {
-                Forms form = currencyValue.value >= 1 ? Forms.Bill : Forms.Coin;
-                String img = "src/edu/uca/dhoelzeman/gui/images/" + currencyValue.name + ".png";
-                var denomination = new Denomination(currencyValue.name, currencyValue.value, form, img);
+                var denomination = new Denomination(currencyValue.name, currencyValue.value, currencyValue.form, currencyValue.img);
                 purse.add(denomination, num);
             }
 
+            // Update amount after adding the denomination to the purse
             amount = remainingAmount;
         }
 
